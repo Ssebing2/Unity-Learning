09e26db2f2e8e7a388ef073b4172bef4cdb8bc84
요셉 <yoseb0908@gmail.com>
2026-08-07 01:13:07 +0900

    Day06_플레이어시야구현(진행중)

--- Day06_플레이어 시야구현(진행중)/Assets/Unity_Day08_Don't Get Caught/PlayerandCamerMove.cs
+++ Day06_플레이어 시야구현(진행중)/Assets/Unity_Day08_Don't Get Caught/PlayerandCamerMove.cs
@@ -1,69 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class PlayerandCamerMove : MonoBehaviour
 {
     [Header("플레이어 움직임")]
-    [SerializeField] private float _movespeed = 5.0f;
-    [SerializeField] private float _rotationSpeed = 100.0f;
+    [SerializeField] private float _moveSpeed = 20.0f;
 
-    private void Start()
-    {
-        
-    }
+    private Vector3 _moveDirection;
+    private bool _isMoving;
 
     private void Update()
     {
+        CheckDirectionInput();
         PlayerMoving();
-        PlayerRotation();
+    }
+
+    private void CheckDirectionInput()
+    {
+        // 키를 처음 누른 순간에만 이동 방향을 결정한다.
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SetDirection(transform.forward);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            SetDirection(-transform.forward);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            SetDirection(-transform.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            SetDirection(transform.right);
+        }
+
+        // 방향키 중 하나라도 누르고 있으면 이동
+        _isMoving =
+            Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.D);
+    }
+
+    private void SetDirection(Vector3 direction)
+    {
+        direction.y = 0f;
+        _moveDirection = direction.normalized;
+
+        // Player 전체 회전
+        // 자식인 몸과 카메라도 함께 돌아간다.
+        transform.forward = _moveDirection;
     }
 
     private void PlayerMoving()
     {
-        Vector3 moveDirection = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.W))
+        if (!_isMoving)
         {
-            moveDirection += transform.forward;
+            return;
         }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            moveDirection -= transform.forward;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveDirection -= transform.right;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveDirection += transform.right;
-        }
-
-        moveDirection = moveDirection.normalized;
-
-        transform.position += moveDirection * _movespeed * Time.deltaTime;
+        transform.position += _moveDirection * _moveSpeed * Time.deltaTime;
     }
-
-    private void PlayerRotation()
-    {
-        float rotationDirection = 0f;
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rotationDirection = -1f;
-        }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            rotationDirection = 1f;
-        }
-
-        transform.Rotate(Vector3.up, rotationDirection * _rotationSpeed * Time.deltaTime);
-    }
-
 }
